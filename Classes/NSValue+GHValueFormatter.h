9f582e8a5b97f5b214e4490d3b1a7391d6c88952
Johannes Rudolph <jr@JRMacMini.fritz.box>
2009-09-23 17:05:38 +0200

    Added NSValue+GHValueFormatter to print values in a nice way. Source from SentestingKit.

---
+++ Classes/NSValue+GHValueFormatter.h
@@ -0,0 +1,16 @@
+//
+//  GHValueFormatter.h
+//  GHUnitIPhone
+//
+//  Created by Johannes Rudolph on 23.09.09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface GHValueFormatter : NSObject {
+
+}
+
+@end
---
+++ Classes/NSValue+GHValueFormatter.m
@@ -0,0 +1,14 @@
+//
+//  GHValueFormatter.m
+//  GHUnitIPhone
+//
+//  Created by Johannes Rudolph on 23.09.09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import "GHValueFormatter.h"
+
+
+@implementation GHValueFormatter
+
+@end
